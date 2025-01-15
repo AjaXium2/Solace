b9a1824469ec25c2131317407d9641ea18e9f4fe
EKrobert <robertkodjoezian@gmail.coom>
2025-01-15 10:38:13 +0100

    Voice function

--- server/apis/voice.py
+++ server/apis/voice.py
@@ -3,8 +3,8 @@ from elevenlabs.client import ElevenLabs
 import soundfile as sf
 import os
 
-# Définir directement la clé API (pas besoin d'utiliser set ici)
-VOICE_API_KEY = "sk_e26404d74b6c4c02cbcb526ccac90a0323a58512152ff57e"
+# la clé API
+VOICE_API_KEY = "sk_f65e4c6c051524204a876096c9aa676e64388b98e2393e6a"
 
 # Initialiser le client Eleven Labs avec la clé API
 client = ElevenLabs(
